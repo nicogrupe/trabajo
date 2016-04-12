3a1ebc0f0654bf73ecb3fbaee9d17a0a6ee8c50d
Martin <martindionisi97@hotmail.com>
2016-04-12 20:43:54 -0300

    tp

--- agenda/main.cpp
+++ agenda/main.cpp
@@ -1,87 +1,51 @@
-//
-//  main.cpp
-//  agenda
-//
-//  Created by martin dionisi on 5/4/16.
-//  Copyright (c) 2016 martin dionisi. All rights reserved.
-//
-
 #include <iostream>
-#include <string.h>
 
 using namespace std;
 
-struct Contacto{
-    char nombre[100];
-    char numero[100];
+struct palabras{
+    
+    char *nombres[5];
 };
 
-struct Contacto contactos[3];
-
-void agregarContactos()
+void Ordenar(char*s[])
 {
-    for(int i=0;i<3;i++)
+    char *aux;
+    for(int i = 0;i<4;i++)
     {
-        cout << "Nombre nº" << i+1 << endl;
-        cin >> contactos[i].nombre;
-        cout << "Telefono nº" << i+1<< endl;
-        cin >> contactos[i].numero;
+        for(int i = 0;i<4;i++)
+        {
+            if( strcmp(s[i],s[i+1]) > 0){
+                aux = s[i+1];
+                s[i+1] = s[i];
+                s[i] = aux;
+            }
+        
+        }
     }
 }
 
-bool porNombre()
+int main()
 {
-    char n[100];
-    cout << "Ingrese nombre: ";
-    cin >> n;
-    for(int i=0;i<3;i++)
-    {
-        if(!strcmp(n,contactos[i].nombre)){
-            cout << "El numero correspondiente es: " << contactos[i].numero << endl;
-            return true;
-        }
+    
+    palabras lista;
+    
+    for(int i=0;i < 5;i++){
+        cout << "ingrese nombre nº" << i+1 << endl;
+        lista.nombres[i] = new char[0];
+        cin >> lista.nombres[i];
     }
-    cout << "no hay resultados" << endl;
-    return false;
-}
-
-bool porNumero()
-{
-    char n[100];
-    cout << "Ingrese telefono: ";
-    cin >> n;
-    for(int i=0;i<3;i++)
-    {
-        if(!strcmp(n,contactos[i].numero)){
-            cout << "El nombre correspondiente es: " << contactos[i].nombre << endl;
-            return true;
+    
+    Ordenar(lista.nombres);
+    
+    cout << "nombres ordenados: " << endl;
+    
+    for(int i = 0;i<5;i++){
+        for(int j =0;j<strlen(lista.nombres[i]);j++){
+            cout << lista.nombres[i][j];
         }
+        cout << endl;
     }
-    cout << "no hay resultados" << endl;
-    return false;
     
-}
-
-int main(int argc, const char * argv[]) {
     
-    bool running = true;
-    int menu;
-    
-    agregarContactos();
-    
-    while(running)
-    {
-        cout << "1)Bucar por nombre" << endl << "2)Buscar por telefono" << endl;
-        cin >> menu;
-        switch(menu){
-            case 1:
-                porNombre();
-                break;
-            case 2:
-                porNumero();
-                break;
-        }
-    }
     return 0;
-    
 }
